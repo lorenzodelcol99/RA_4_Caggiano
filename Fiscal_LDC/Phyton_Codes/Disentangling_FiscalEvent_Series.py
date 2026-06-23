96f1b4ff78330f0df2862675417243daab5fe205
lorenzodelcol99 <lorenzodelcol99@gmail.com>
2026-06-23 10:19:59 +0200

    finally, works, the problem was related to the path. Now I wark in Python while using the REPL as console

--- Fiscal_LDC/Phyton_Codes/Disentangling_FiscalEvent_Series.py
+++ Fiscal_LDC/Phyton_Codes/Disentangling_FiscalEvent_Series.py
@@ -1,12 +1,15 @@
+#%%
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
+#%%
 # Define the file name and column names
-FILE_NAME = 'Disentanglying_Fiscal_Event_Daily_Series.xlsx'
+FILE_NAME = 'Disentangling_FiscalEvent_Series.xlsx'
 DATE_COLUMN = 'Date'
 VALUE_COLUMN = 'pv_change_fiscal_events'
 
+#%%
 # Load the Excel file
 df = pd.read_excel(FILE_NAME)
-df.head()
+print(df.head())
+# %%
