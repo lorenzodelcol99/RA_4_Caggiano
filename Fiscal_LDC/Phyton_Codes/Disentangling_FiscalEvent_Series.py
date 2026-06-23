8877a77ac762d45c41a8a95994ce2ad08ca4bf0f
lorenzodelcol99 <lorenzodelcol99@gmail.com>
2026-06-23 10:26:03 +0200

    updated path and fixed name of excel

--- Fiscal_LDC/Phyton_Codes/Disentangling_FiscalEvent_Series.py
+++ Fiscal_LDC/Phyton_Codes/Disentangling_FiscalEvent_Series.py
@@ -1,10 +1,15 @@
 #%%
 import pandas as pd
 import numpy as np
+import os
+
+# Stampa la directory corrente per farti vedere dove Python crede di trovarsi
+print("Directory di lavoro attuale:", os.getcwd())
+
 
 #%%
 # Define the file name and column names
-FILE_NAME = 'Disentangling_FiscalEvent_Series.xlsx'
+FILE_NAME = '../Disentangling_Fiscal_Event_Daily_Series.xlsx'
 DATE_COLUMN = 'Date'
 VALUE_COLUMN = 'pv_change_fiscal_events'
 
